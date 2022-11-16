6ffed6170289aaa6588ea4c05e762965d15e8ab6
Yu-Ren Zhang <e8035669@gmail.com>
2022-11-16 13:48:56 +0800

    fix det_id not update

--- yolox/tracker/byte_tracker.py
+++ yolox/tracker/byte_tracker.py
@@ -68,6 +68,7 @@ class STrack(BaseTrack):
         if new_id:
             self.track_id = self.next_id()
         self.score = new_track.score
+        self.det_id = new_track.det_id
 
     def update(self, new_track, frame_id):
         """
